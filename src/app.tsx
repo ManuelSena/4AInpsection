e1263ff3e9b6bc38f2b1b0595f040e9000cfbb39
ManuelS <manuelsena@me.com>
2020-07-06 12:11:49 -0700

    Update app.tsx

--- src/app.tsx
+++ src/app.tsx
@@ -4,7 +4,7 @@ import { Header } from "./components/header/header";
 export const App: React.StatelessComponent<{}> = (props) => {
 
     return (
-        <div>
+        <div id="root">
             <Header />
             {props.children}
         </div>
